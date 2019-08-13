f0fc95c1e275a40e62914301aea8941f71b3463c
kangajos <aguss6460@gmail.com>
2019-08-13 09:42:07 +0700

    update login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ( isset($_SESSION['apriori_toko_id']) ) {
+if (isset($_SESSION['apriori_toko_id'])) {
     header("location:index.php");
 }
 
@@ -17,108 +17,120 @@ if ($login == 1) {
 include_once "fungsi.php";
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <title>Login - Apriori toko</title>
-        <link href="images/icon/login.gif" rel="shortcut icon" />
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 2 | Simple Tables</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css">
 
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
-        <!--
-        Artcore Template
-		http://www.templatemo.com/preview/templatemo_423_artcore
-        -->
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="scripts/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="css/font-awesome.css">
-        <link rel="stylesheet" href="css/animate.css">
-        <link rel="stylesheet" href="css/templatemo-misc.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
-        <link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href="scripts/toast/jquery.toast.min.css">
-        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-        <![endif]-->
+    <!-- Google Font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-        <section id="pageloader">
-            <div class="loader-item fa fa-spin colored-border"></div>
-        </section> <!-- /#pageloader -->
+</head>
+<!--<body>-->
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser
+    today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better
+    experience this site.</p>
+<![endif]-->
 
-        <?php
+<section id="pageloader">
+    <div class="loader-item fa fa-spin colored-border"></div>
+</section> <!-- /#pageloader -->
+
+<?php
 //        include "header.php";
+?>
+
+<body class="hold-transition register-page">
+<div class="register-box">
+    <div class="register-logo">
+        <a href="../../index2.html"><b>LOGIN</b>ADMIN</a>
+    </div>
+
+    <div class="register-box-body">
+        <p class="login-box-msg">Silahkan login</p>
+        <!-- <h3 class="contact-title">Send Us Email</h3> -->
+        <?php
+        if (isset($komen)) {
+            display_error("Login failed");
+        }
         ?>
+        <!--                                <div class="contact-form-inner">-->
+        <form method="post" action="cek-login.php">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input name="username" type="text" id="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input name="password" type="password" id="password" class="form-control">
+            </div>
+            <div class="form-gruop">
+                <input type="submit" class="btn btn-primary btn-block" id="submit" value="Login"/>
+            </div>
+        </form>
 
-        <div class="content-wrapper">
-            <div class="inner-container container">
-                <div class="row">
-                    <div class="section-header col-md-12">
-                        <h2>Login Page</h2>
-                        <!-- <span>Subtitle Goes Here</span> -->
-                    </div> <!-- /.section-header -->
-                </div> <!-- /.row -->
-                <div class="contact-form">
-                    <div class="box-content col-md-12">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <p></p>
-                                <!-- <h3 class="contact-title">Send Us Email</h3> -->
-                                <?php
-                                if (isset($komen)) {
-                                    display_error("Login failed");
-                                }
-                                ?>
-                                <div class="contact-form-inner">
-                                    <form method="post" action="cek-login.php" >
-                                        <p>
-                                            <label for="username">Username:</label>
-                                            <input name="username" type="text" id="username">
-                                        </p>
-                                        <p>
-                                            <label for="password">Password:</label>
-                                            <input name="password" type="password" id="password">   
-                                        </p>
-                                        
-                                        <input type="submit" class="mainBtn" id="submit" value="Login" />
-                                    </form> 
+    </div>
+    <!-- /.form-box -->
+</div>
+<script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="assets/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/dist/js/adminlte.min.js"></script>
+<!-- Sparkline -->
+<script src="assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap  -->
+<script src="assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- SlimScroll -->
+<script src="assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="assets/bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets/dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="assets/dist/js/demo.js"></script>
+<script src="js/vendor/jquery-1.11.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<script type="text/javascript" src="scripts/bootstrap/bootstrap.min.js"></script>
 
-                                </div> <!-- /.contact-form-inner -->
-                                <!-- <div id="message"></div> -->
-                            </div> <!-- /.col-md-7 -->
-                            
-                        </div> <!-- /.row -->
-                    </div> <!-- /.box-content -->
-                </div> <!-- /.contact-form -->
-            </div> <!-- /.inner-content -->
-        </div> <!-- /.content-wrapper -->
+<!-- Preloader -->
+<script type="text/javascript">
+    //<![CDATA[
+    $(window).load(function () { // makes sure the whole site is loaded
+        $('.loader-item').fadeOut(); // will first fade out the loading animation
+        $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({'overflow-y': 'visible'});
+    })
+    //]]>
+</script>
 
-        <script src="js/vendor/jquery-1.11.0.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        
-        
-        
-        <!-- Preloader -->
-        <script type="text/javascript">
-            //<![CDATA[
-            $(window).load(function() { // makes sure the whole site is loaded
-                $('.loader-item').fadeOut(); // will first fade out the loading animation
-                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-                $('body').delay(350).css({'overflow-y':'visible'});
-            })
-            //]]>
-        </script>
-
-    </body>
+</body>
 </html>
